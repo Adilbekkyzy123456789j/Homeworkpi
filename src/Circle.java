ba688e596c12c2baa5062b301f3fcf13569eff31
Adilb <iskendermurzattillauulu@gmail.com>
2023-07-19 16:07:43 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    static final  double PI=3.14;
+
+    public static double Area(double radius2) {
+    return PI*radius2*radius2;
+
+    }
+    public static double circumference(double radius3){
+        return PI*2*radius3;
+
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Circle.Area(5));
+        System.out.println(Circle.circumference(5));
+
+    }
+}
